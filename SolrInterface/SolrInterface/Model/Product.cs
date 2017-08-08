f844b62a7a0a7ac6f6a82be0a3e469861d024889
Glendon Cheney <glendon.cheney@gmail.com>
2017-08-08 16:02:31 -0500

    Add product model

---
+++ SolrInterface/SolrInterface/Model/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SolrNet.Attributes;
+
+namespace SolrInterface.Model
+{
+    public class Product
+    {
+        [SolrUniqueKey("id_text")]
+        public string Id { get; set; }
+
+        [SolrField("product_count_integer")]
+        public int ProductCount { get; set; }
+
+        [SolrField("title_text")]
+        public string Title { get; set; }
+
+        [SolrField("created_on_datetime")]
+        public DateTime CreatedOn { get; set; }
+
+        [SolrField("price_decimal")]
+        public decimal Price { get; set; }
+
+        [SolrField("inStock_boolean")]
+        public bool InStock { get; set; }
+    }
+}
